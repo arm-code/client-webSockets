89acd30392b62f66a46615e5da8dc0c98630bb51
Alexis Romero Mendoza <alexis.rinnegan@gmail.com>
2025-06-27 13:21:17 -0600

    feat: client form, sockets, jwt

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,13 +1,23 @@
 import { Manager, Socket } from "socket.io-client"
 
-export const connectToServer = () => {
-    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
+export const connectToServer = ( token: string) => {
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders: {
+            hola: 'mundo',
+            authentication:  token
+        }
+    });
     const socket = manager.socket('/')
     addListener(socket)
 }
 
 const addListener = ( socket: Socket) => {
     const serverStatusLabel = document.querySelector('#server-status')!;
+    const clientsUl = document.querySelector('#clients-ul')!;
+    const messagesUl = document.querySelector('#messages-ul')
+
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLFormElement>('#message-input')!;
 
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'connected'
@@ -17,4 +27,37 @@ const addListener = ( socket: Socket) => {
         serverStatusLabel.innerHTML = 'disconnected'
     })
 
+    socket.on('clients-updated', (clients: string []) => {
+        let clientsHtml = ''
+        clients.forEach( clientId => {
+            clientsHtml += `<li>${ clientId }</li>`
+        })
+
+        clientsUl.innerHTML = clientsHtml;
+    })
+
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if( messageInput.value.trim().length <= 0) return
+        socket.emit('message-from-client', { 
+            id: 'YO', 
+            message: messageInput.value 
+        })
+
+        messageInput.value = ''
+    })
+
+    socket.on('message-from-server', (payload: {fullName: string, message:string}) => {
+        const newMessage = `
+        <li>
+            <strong>${ payload.fullName }</strong>
+            <span>${ payload.message }</span>
+        </li>
+        `
+
+        const li = document.createElement('li')
+        li.innerHTML = newMessage;
+        messagesUl?.append(li)
+
+    })
 }
